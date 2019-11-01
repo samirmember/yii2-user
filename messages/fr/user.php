b4c5d37d9af10a01eb11c9b145ede3bd65e1a44b
Samir IZZA <samirmember@gmail.com>
2019-11-01 21:35:46 +0100

    Update user.php

--- messages/fr/user.php
+++ messages/fr/user.php
@@ -43,7 +43,7 @@ return array (
   'Time zone is not valid' => '',
   'Your account has been blocked.' => '',
   'Your account has been completely deleted' => '',
-  'Your account has been created and a message with further instructions has been sent to your email' => '',
+  'Your account has been created and a message with further instructions has been sent to your email' => 'Votre compte a été créé et un message contentn les instructions d\'activation a été envoyé sur votre boîte mail',
   'Your account on {0} has a new password' => '',
   'Your password on {0} has been changed' => '',
   '(not set)' => '(non défini)',
